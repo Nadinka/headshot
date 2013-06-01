db37e76719febccd272032d3f3cdfe20b7f7a5b4
Nadinka <dubitskayanadia@gmail.com>
2013-06-02 00:48:01 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headshot::Application.config.secret_token = '57b55bce7784340c9192e9b4064539c81f193e548455943e015fb972caa45950a71b895aae744804805b7123fedd473355cad5ced15976d533d6d0d0027b3382'
